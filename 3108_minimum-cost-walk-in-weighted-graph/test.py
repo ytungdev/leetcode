e247fef0615908cc5872c4c3bf79e59e8f086da2
Yi <yi@Camellias-Air.communityfibre.co.uk>
2025-03-21 17:36:34 +0000

    add testing script

---
+++ 3108_minimum-cost-walk-in-weighted-graph/test.py
@@ -0,0 +1,96 @@
+from typing import List
+
+
+class UF:
+    def __init__(self, N):
+        self.__root = [i for i in range(N)]
+        self.__size = [1] * N
+        self.__cost = [-1] * N
+
+    def find_root(self, p):
+        if self.__root[p] == p:
+             return p
+        return self.find_root(self.__root[p])
+
+    def add_edge(self, p, q, w):
+        p_root = self.find_root(p)
+        q_root = self.find_root(q)
+        if p_root != q_root:
+            if self.__size[p_root] < self.__size[q_root]:
+                print(f'{p_root} -> {q_root}')
+                self.__root[p_root] = q_root
+                self.__size[q_root] += self.__size[p_root]
+                self.__cost[q_root] &= self.__cost[p_root]
+                self.__cost[q_root] &= w
+            else:
+                print(f'{q_root} -> {p_root}')
+                self.__root[q_root] = p_root
+                self.__size[p_root] += self.__size[q_root]
+                self.__cost[p_root] &= self.__cost[q_root]
+                self.__cost[p_root] &= w
+        else:
+            self.__cost[p_root] &= w
+        print(f'join : {p,q,w}')
+        self.print()
+        print()
+
+
+    def find_cost(self,p):
+        root = self.find_root(p)
+        return self.__cost[root]
+    
+    def print(self):
+        print(f'uf:{self.__root}, ws:{self.__cost}, sz:{self.__size}')
+
+class Solution:
+
+    def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
+        uf = UF(n)
+        res = []
+
+        for p,q,w in edges:
+            uf.add_edge(p,q,w)
+
+        for p,q in query:
+            p_root = uf.find_root(p)
+            q_root = uf.find_root(q)
+            
+            if p==q:
+                ans = 0
+            elif p_root == q_root:
+                ans = uf.find_cost(p)
+            else:
+                ans = -1
+            res.append(ans)
+        uf.print()
+        return res
+
+
+sol = Solution()
+
+test = [
+    [5,[[0,1,7],[1,3,7],[1,2,1]],[[0,3],[3,4]], [1,-1]],
+    [3,[[0,2,7],[0,1,15],[1,2,6],[1,2,1]],[[1,2]], [0]],
+    [5,[[0,1,7],[3,4,7],[1,2,1]],[[0,3],[3,4]], [-1,7]],
+    [4,[[2,3,1],[1,3,5],[1,2,6],[3,0,7],[1,3,7],[0,2,5],[0,1,7]],[[2,1],[1,2],[0,1],[2,0],[0,2],[1,2],[3,2],[0,3],[2,1],[1,2]], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+    [5,[[0,1,7],[1,3,7],[1,2,1]],[[0,3],[3,4]], [1,-1]],
+    [7, [[3,0,2],[5,4,12],[6,3,7],[4,2,2],[6,2,2]], [[6,0]], [0]]
+]
+
+# for t in test:
+#     n, edges, query, expect = t
+#     print(f'e : {edges}, q : {query}')
+#     res = sol.minimumCost(n, edges, query)
+#     print(f'res : {res}, exp : {expect}\n')
+
+n, edges, query, exp = test[-1]
+res = sol.minimumCost(n, edges, query)
+print(f'res : {res}, exp : {exp}')
+
+'''
+[6]--07--[3]--02--[0]
+|
+02
+|
+[2]--02--[5]--12--[4]
+'''
