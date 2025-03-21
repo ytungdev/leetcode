c2c205447427fe53b194ecdce5031234ae80ae70
ytung.dev@PC <ytung.dev@gmail.com>
2025-03-21 23:47:31 +0000

    added solution-v2

--- 3108_minimum-cost-walk-in-weighted-graph/solution-v2.py
+++ 3108_minimum-cost-walk-in-weighted-graph/solution-v2.py
@@ -1,5 +1,4 @@
-# Beat 16.19%
-
+# Beat 29.8%
 from typing import List
 
 class UF:
@@ -10,33 +9,34 @@ class UF:
 
 
     def find_root(self, p):
-        if self.__root[p] == p:
-             return p
-        return self.find_root(self.__root[p])
+        if self.__root[p] != p:
+            # path compression
+            self.__root[p] = self.find_root(self.__root[p])
+        return self.__root[p]
 
     def add_edge(self, p, q, w):
         p_root = self.find_root(p)
         q_root = self.find_root(q)
+        
+        self.__cost[p_root] &= w
+        self.__cost[q_root] &= w
+        
         if p_root != q_root:
             if self.__size[p_root] < self.__size[q_root]:
-                print(f'{p_root} -> {q_root}')
                 self.__root[p_root] = q_root
                 self.__size[q_root] += self.__size[p_root]
                 self.__cost[q_root] &= self.__cost[p_root]
-                self.__cost[q_root] &= w
             else:
-                print(f'{q_root} -> {p_root}')
                 self.__root[q_root] = p_root
                 self.__size[p_root] += self.__size[q_root]
                 self.__cost[p_root] &= self.__cost[q_root]
-                self.__cost[p_root] &= w
-        else:
-            self.__cost[p_root] &= w
+            
     def find_cost(self,p):
         root = self.find_root(p)
         return self.__cost[root]
 
 class Solution:
+
     def minimumCost(self, n: int, edges: List[List[int]], query: List[List[int]]) -> List[int]:
         uf = UF(n)
         res = []
@@ -45,14 +45,13 @@ class Solution:
             uf.add_edge(p,q,w)
 
         for p,q in query:
-            p_root = uf.find_root(p)
-            q_root = uf.find_root(q)
-            
             if p==q:
                 ans = 0
-            elif p_root == q_root:
-                ans = uf.find_cost(p)
             else:
-                ans = -1
+                if uf.find_root(p) != uf.find_root(q):
+                    ans = -1
+                else:
+                    ans = uf.find_cost(p)
             res.append(ans)
         return res
+
--- 3108_minimum-cost-walk-in-weighted-graph/test.py
+++ 3108_minimum-cost-walk-in-weighted-graph/test.py
@@ -8,36 +8,35 @@ class UF:
         self.__cost = [-1] * N
 
     def find_root(self, p):
-        if self.__root[p] == p:
-             return p
-        return self.find_root(self.__root[p])
+        if self.__root[p] != p:
+            # path compression
+            self.__root[p] = self.find_root(self.__root[p])
+        return self.__root[p]
+    
+        
 
     def add_edge(self, p, q, w):
         p_root = self.find_root(p)
         q_root = self.find_root(q)
-        if p_root != q_root:
-            if self.__size[p_root] < self.__size[q_root]:
-                print(f'{p_root} -> {q_root}')
-                self.__root[p_root] = q_root
-                self.__size[q_root] += self.__size[p_root]
-                self.__cost[q_root] &= self.__cost[p_root]
-                self.__cost[q_root] &= w
-            else:
-                print(f'{q_root} -> {p_root}')
-                self.__root[q_root] = p_root
-                self.__size[p_root] += self.__size[q_root]
-                self.__cost[p_root] &= self.__cost[q_root]
-                self.__cost[p_root] &= w
-        else:
-            self.__cost[p_root] &= w
         print(f'join : {p,q,w}')
+
+        if p_root == q_root:
+            self.__cost[p_root] &= w
+            return
+    
+        if self.__size[p_root] < self.__size[q_root]:
+            p_root, q_root = q_root, p_root
+        
+        print(f'{q_root} -> {p_root}')
+        
+        self.__root[q_root] = p_root
+        self.__cost[p_root] &= (self.__cost[q_root] & w)
+        self.__size[p_root] += self.__size[q_root]
         self.print()
-        print()
 
 
     def find_cost(self,p):
-        root = self.find_root(p)
-        return self.__cost[root]
+        return self.__cost[p]
     
     def print(self):
         print(f'uf:{self.__root}, ws:{self.__cost}, sz:{self.__size}')
@@ -52,18 +51,17 @@ class Solution:
             uf.add_edge(p,q,w)
 
         for p,q in query:
-            p_root = uf.find_root(p)
-            q_root = uf.find_root(q)
-            
             if p==q:
                 ans = 0
-            elif p_root == q_root:
-                ans = uf.find_cost(p)
             else:
-                ans = -1
+                if uf.find_root(p) != uf.find_root(q):
+                    ans = -1
+                else:
+                    ans = uf.find_cost(uf.find_root(p))
             res.append(ans)
         uf.print()
         return res
+
 
 
 sol = Solution()
@@ -77,15 +75,15 @@ test = [
     [7, [[3,0,2],[5,4,12],[6,3,7],[4,2,2],[6,2,2]], [[6,0]], [0]]
 ]
 
-# for t in test:
-#     n, edges, query, expect = t
-#     print(f'e : {edges}, q : {query}')
-#     res = sol.minimumCost(n, edges, query)
-#     print(f'res : {res}, exp : {expect}\n')
+for t in test:
+    n, edges, query, expect = t
+    print(f'e : {edges}, q : {query}')
+    res = sol.minimumCost(n, edges, query)
+    print(f'res : {res}, exp : {expect}\n{"-"*50}\n\n')
 
-n, edges, query, exp = test[-1]
-res = sol.minimumCost(n, edges, query)
-print(f'res : {res}, exp : {exp}')
+# n, edges, query, exp = test[-1]
+# res = sol.minimumCost(n, edges, query)
+# print(f'res : {res}, exp : {exp}')
 
 '''
 [6]--07--[3]--02--[0]
